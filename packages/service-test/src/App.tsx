9001069737f759d5f69f100b044b85969315b075
sinhu <tlsgn26@naver.com>
2024-05-22 11:41:41 +0900

    모노레포로 기초적인 셋팅 해보기

---
+++ packages/service-test/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from "@monorepo/test-project";
+
+function App() {
+  return (
+    <div>
+      <Button>test</Button>
+    </div>
+  );
+}
+
+export default App;
